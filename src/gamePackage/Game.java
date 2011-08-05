fd88a861b7910245699d855d02c052bdca679193
Okan <okan.giritli@gmail.com>
2011-08-05 15:13:06 +0300

    m

--- src/gamePackage/Game.java
+++ src/gamePackage/Game.java
@@ -2,6 +2,8 @@ package gamePackage;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -12,7 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Game extends JFrame implements ActionListener{
+public class Game extends JFrame implements ActionListener, WindowListener{
 
 	/**
 	 * 
@@ -26,12 +28,13 @@ public class Game extends JFrame implements ActionListener{
 	private PrintWriter out;
 	
 	public Game(){
-		new ReaderThread(in).start();
 		try{
 			socket = new Socket("localhost", 12345);
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new PrintWriter(socket.getOutputStream());
+			new ReaderThread(in).start();
 		}catch (Exception e) {
+			e.printStackTrace();
 		}
 		commandField = new JTextField(20);
 		commandField.addActionListener(this);
@@ -41,6 +44,7 @@ public class Game extends JFrame implements ActionListener{
 		JPanel p = new JPanel();
 		p.add(commandField);
 		p.add(sendBtn);
+		addWindowListener(this);
 		setContentPane(p);
 		pack();
 		setVisible(true);
@@ -50,6 +54,51 @@ public class Game extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		out.println(commandField.getText());
 		out.flush();
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		try{
+			socket.close();
+		}catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
